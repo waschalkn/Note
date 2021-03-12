3ba1fc086409ef557b69146b9742c66d9f33e9b9
Nacoda Waschalk <nacodaw93@gmail.com>
2021-03-12 10:33:44 -0700

    Added Note component to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
+import Note from './components/Note';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <List />
+      <Note />
     </div>
   );
 }
