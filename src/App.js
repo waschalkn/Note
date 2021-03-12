b459aba233bbff958c36ce1123fad8fdbd9e8970
Nacoda Waschalk <nacodaw93@gmail.com>
2021-03-12 10:29:03 -0700

    Added List component to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <List />
     </div>
   );
 }
